79fa681ec64a9b1fb813b3849cbd3c7540154f2c
RameshNaik2021 <2018uch1661@mnit.ac.in>
2024-06-21 17:26:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React, { useState } from 'react';
+import './App.css';
+import axios from 'axios'
+import Gallery from './Gallery';
+
+const apiKey = "636e1481b4f3c446d26b8eb6ebfe7127";
 function App() {
+  const [data, setdata] = useState([]);
+  const [search, setSearch] = useState("");
+ 
+  
+
+  const handlechanger = e => {
+    setSearch(e.target.value)
+  }
+  const submithandler = e => {
+    e.preventDefault()
+    setSearch("")
+
+  
+  axios
+  .get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${search}&per_page=24&format=json&nojsoncallback=1`)
+  .then(res=>{setdata(res.data.photos.photo)})
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <h2>gallery</h2>
+      <form onSubmit={submithandler}>
+        <input type="text" value={search} onChange={handlechanger} /> <br /> <br />
+        <input type="submit" />
+      </form>
+      <br />
+      <br />
+      {data.length>=1?<Gallery data={data}/>:<h2>no data found</h2>}
+      
+     
+    </center>
+
   );
 }
 
